fd6154f973eb71fc7a2527e32c94b5f7ecfed297
Richard Dobroň <office.etexweb@gmail.com>
2022-05-06 20:23:43 +0200

    :sparkles: Add unique node ID generator

--- src/helpers.php
+++ src/helpers.php
@@ -10,3 +10,23 @@ function array_trim(array $array): array
 
     return $array;
 }
+
+/**
+ * Generate a node ID which is guaranteed to be unique for the current page,
+ * even across Ajax requests. You should use this method to generate IDs for
+ * nodes which require a uniqueness guarantee.
+ *
+ * @return string A string appropriate for use as an 'id' attribute on a DOM
+ *                node. It is guaranteed to be unique for the current page, even
+ *                if the current request is a subsequent Ajax request.
+ */
+function generate_unique_node_id(): string
+{
+    static $unique = 0;
+
+    return implode('_', [
+        'u',
+        $_REQUEST['__req'] ?? 0,
+        $unique++,
+    ]);
+}
